c7b7437cd933897993a2343bddbe0a7ffc4f9b19
Nicolas Velasquez <nicolas.velasquez1233@gmail.com>
2026-02-02 00:20:21 +0100

    feat: sprint1 ping endpoint, security, postgres, docker, tests

---
+++ src/test/java/com/nicolas/appsec/api/PingControllerTest.java
@@ -0,0 +1,28 @@
+package com.nicolas.appsec.api;
+
+import com.nicolas.appsec.security.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(PingController.class)
+@Import(SecurityConfig.class)
+class PingControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void ping_returns_ok() throws Exception {
+        mvc.perform(get("/api/v1/ping"))
+           .andExpect(status().isOk())
+           .andExpect(jsonPath("$.status").value("ok"))
+           .andExpect(jsonPath("$.service").value("appsec-java-core"))
+           .andExpect(jsonPath("$.time").exists());
+    }
+}

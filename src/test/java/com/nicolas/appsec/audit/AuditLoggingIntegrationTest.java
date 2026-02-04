f30aea52da7079e1dc9007461920203aef94c4ac
Nicolas Velasquez <nicolas.velasquez1233@gmail.com>
2026-02-04 23:07:08 +0100

    hardening: dev auth, actor normalization and trusted client ip

--- src/test/java/com/nicolas/appsec/audit/AuditLoggingIntegrationTest.java
+++ src/test/java/com/nicolas/appsec/audit/AuditLoggingIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.nicolas.appsec.audit;
 
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@EnabledIfEnvironmentVariable(named = "RUN_TC", matches = "true")
 @Testcontainers
 @SpringBootTest
 @AutoConfigureMockMvc
